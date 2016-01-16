329b83b1c47be003314be5102a16c50cbf1d7150
tom <tom.zhu1119@gmail.com>
2016-01-16 16:08:25 +0800

    m

--- part1/webpack.config.js
+++ part1/webpack.config.js
@@ -1,7 +1,9 @@
 module.exports={
   entry:'./index.js',
-  output:__dirname,
-  filename:'bundle.js',
+  output:{
+    path:__dirname,
+    filename:'bundle.js'
+  },
   module:{
     loaders:[
       {
